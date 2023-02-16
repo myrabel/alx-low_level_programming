e4bc2843eaa5d1c7d0a0855d8c319824b210125f
myrabel <memusi2005@gmail.com>
2023-02-16 12:05:14 -0800

    Sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
-/*print the size of various types on the computer this code
- * is compliled on
- */
-int main(void)
+/*print the size of various types on the computer this code is compiled on */
+int main(size)
 {
 	int intType;
 	float floatType;
@@ -11,10 +9,10 @@ int main(void)
 	long long int longlongType;
 
 	/*evaluate size of variable */
-	printf("Size of char: %2d byte(s)\n", sizeof(charType));
-	printf("Size of int: %2d byte(s)\n", sizeof(intType));
-	printf("Size of long int: %2d byte(s)\n", sizeof(longintType));
-	printf("Size of long long int %2d byte(s)\n", sizeof(longlongType));
-	printf("Size of float: %2d byte(s)\n", sizeof(floatType));
+	printf("Size of a char: %2d byte(s)\n", sizeof(charType));
+	printf("Size of a int: %2d byte(s)\n", sizeof(intType));
+	printf("Size of a long int: %2d byte(s)\n", sizeof(longintType));
+	printf("Size of a long long int %2d byte(s)\n", sizeof(longlongType));
+	printf("Size of a float: %2d byte(s)\n", sizeof(floatType));
 	return (0);
 }

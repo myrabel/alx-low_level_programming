45e78eb7e1d4390583f3b76596e4fce07a33753a
myrabel <memusi2005@gmail.com>
2023-02-16 12:26:05 -0800

    Qoute and escape

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/** main -entry
+ * Description: 'print a quote using printf'
+ * Return 1 on success
+ *
+ */
+
+int main(void)
+{
+	printf("and that piece of art is useful\" -Dora Korpar, 2015-10-19\n");
+	return (1);
+}

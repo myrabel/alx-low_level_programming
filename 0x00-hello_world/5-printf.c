40ae28fafb8aa9f3c464aefcd5311969e4726a3b
myrabel <memusi2005@gmail.com>
2023-02-16 11:36:44 -0800

    Printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,8 +2,8 @@
 /* Display words using printf
  * return: 0
  */
-int main()
+int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art," \n);
+	printf("with proper grammar, but the outcome is a piece of art, \n");
 	return (0);
 }

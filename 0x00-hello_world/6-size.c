11b710eec2785d2033feef85d40f1f32281e38ac
myrabel <memusi2005@gmail.com>
2023-02-16 12:10:14 -0800

    Sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,7 +12,7 @@ int main(size)
 	printf("Size of a char: %d byte(s)\n", sizeof(charType));
 	printf("Size of an int: %d byte(s)\n", sizeof(intType));
 	printf("Size of a long int: %d byte(s)\n", sizeof(longintType));
-	printf("Size of a long long int %d byte(s)\n", sizeof(longlongType));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(longlongType));
 	printf("Size of a float: %d byte(s)\n", sizeof(floatType));
 	return (0);
 }

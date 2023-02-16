f0b453e0dda015f853748d140de3139bb5d24db2
myrabel <memusi2005@gmail.com>
2023-02-16 11:59:16 -0800

    Find the size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/*print the size of various types on the computer this code
+ * is compliled on
+ */
+int main(void)
+{
+	int intType;
+	float floatType;
+	long int longintType;
+	char charType;
+	long long int longlongType;
+
+	/*evaluate size of variable */
+	printf("Size of char: %2d byte(s)\n", size of(charType));
+	printf("Size of int: %2d byte(s)\n", size of(intType));
+	printf("Size of long int: %2d byte(s)\n", size of(longintType));
+	printf("Size of long long int %2d byte(s)\n", size of(longlongType));
+	printf("Size of float: %2d byte(s)\n", size of(floatType));
+	return (0);
+}

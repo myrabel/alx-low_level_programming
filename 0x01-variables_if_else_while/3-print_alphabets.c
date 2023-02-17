c5921cc8cc3630327d8de340d7ec07501a5840a1
myrabel <memusi2005@gmail.com>
2023-02-17 10:20:16 -0800

    Lower then upper

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'print in lowercase then uppercase'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+
+	for (c = 'A'; c <= 'Z'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}

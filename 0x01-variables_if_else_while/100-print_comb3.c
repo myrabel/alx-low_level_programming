203caaa276fd7cb4942db7a1ceb7d9a67f497761
myrabel <memusi2005@gmail.com>
2023-02-17 12:24:04 -0800

    two digit combos

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'two digit combinations'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a <= 9; a++)
+		for (b = a + 1; b <= 9; b++)
+		{
+			if (a != b)
+				putchar((a) + '0');
+				putchar((b) + '0');
+				putchar(',');
+				putchar(' ');
+		}
+	putchar('\n');
+	return (0);
+}

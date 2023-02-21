93118475a86f4b80fd958bfb72100552f23bf985
myrabel <memusi2005@gmail.com>
2023-02-21 12:58:16 -0800

    print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_last_digit - value of last digit
+ * @int: variable
+ * Return: 0
+ */
+int print_last_digit(int)
+{
+	int n;
+
+	_putchar((n % 10) + '0');
+	return (0);
+}

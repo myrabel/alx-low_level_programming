85d4f3d1721960c02adfe873af88a04726715e27
myrabel <memusi2005@gmail.com>
2023-02-22 10:42:26 -0800

    all naturals

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: compute sum of multiples of 3 and 5
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i, res;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			res += i;
+		printf("%d\n", res);
+		return (0);
+	}
+}

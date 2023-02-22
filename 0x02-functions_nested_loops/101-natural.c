37047844517a7d3e5e59c7a562acaa821a4b6182
myrabel <memusi2005@gmail.com>
2023-02-22 10:45:18 -0800

    all naturals

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -9,10 +9,8 @@ int main(void)
 	int i, res;
 
 	for (i = 0; i < 1024; i++)
-	{
 		if (i % 3 == 0 || i % 5 == 0)
 			res += i;
-		printf("%d\n", res);
-		return (0);
-	}
+	printf("%d\n", res);
+	return (0);
 }

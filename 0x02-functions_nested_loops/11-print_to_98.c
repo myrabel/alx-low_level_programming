80251bb4aca9738d0d1b13112084853296e09b28
myrabel <memusi2005@gmail.com>
2023-02-22 10:15:13 -0800

    print till 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_to_98 - print all natural numbers up to 98
+ * @n: integer variable
+ * Return: last number as 98
+ */
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		for (n = n; n < 98; n++)
+			printf("%d, ", n);
+		printf("%d\n", 98);
+	}
+	else
+	{
+		for (n = n; n < 98; n--)
+		{
+			printf("%d, ", n);
+		printf("%d\n", 98);
+		}
+	}
+}

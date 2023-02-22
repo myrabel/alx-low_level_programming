414487e8d449ba32e1e83f916587a6e9f586890d
myrabel <memusi2005@gmail.com>
2023-02-22 10:04:48 -0800

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,11 +5,31 @@
  */
 void times_table(void)
 {
-	int n, m;
+	int n, m, res;
 
-	if ((n = 0; n <= 9; n++) && (m = 0; m <= 9; m++))
+	for (n = 0; n <= 9; n++)
 	{
-		_putchar((n * m) + '0');
+		for (m = 0; m <= 9; m++)
+		{
+			res = (n * m);
+			if (m != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (res >= 10)
+			{
+				_putchar((res / 10) + '0');
+				_putchar((res % 10) + '0');
+			}
+			else if (res < 10 && m != 0)
+			{
+				_putchar(' ');
+				_putchar((res % 10) + '0');
+			}
+			else
+				_putchar((res % 10) + '0');
+		}
 		_putchar('\n');
 	}
 }

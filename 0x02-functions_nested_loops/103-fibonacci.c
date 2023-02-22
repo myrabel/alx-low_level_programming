7165b64e3df799f570e68df400f1011928e3ea3c
myrabel <memusi2005@gmail.com>
2023-02-22 10:56:09 -0800

    Fibonacci 2

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,27 +1,25 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Description: 'print fibonacci'
+ * Description: 'print 1 to 89 first'
  * Return: Always 0
  */
 int main(void)
 {
-	unsigned long count, i, j, k;
+	unsigned long i, j, k, count, sums;
 
-	i = 0;
+	i = sums = 0;
 	j = 1;
 	for (count = 0; count < 50; count++)
 	{
 		k = i + j;
 		i = j;
 		j = k;
-		printf("%lu", k);
-		if (count == 49)
-			putchar('\n');
-		else
+		if (k % 2 == 0 && k < 4000000)
 		{
-			printf(", ");
+			sums += k;
 		}
 	}
+	printf("%lu\n", sums);
 	return (0);
 }

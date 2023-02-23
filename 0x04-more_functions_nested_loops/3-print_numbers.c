5c1427ac90d9975519c8e90c5ed3427a45c6e5bf
myrabel <memusi2005@gmail.com>
2023-02-23 11:49:40 -0800

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_numbers - print from 0 to 9
+ * Return: Always 0
+ */
+
+void print_numbers(void);
+
+/**
+ * print_numbers - print from 0 to 9
+ * Return: Always 0
+ */
+
+void print_numbers(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar((num) + '0');
+	}
+	_putchar('\n');
+}

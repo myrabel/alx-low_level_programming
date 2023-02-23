f88143599e5bde7b5ebb753a4bbfa27bc0daa5e9
myrabel <memusi2005@gmail.com>
2023-02-23 08:26:34 -0800

    Convert days

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,17 +12,41 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && a > c)
+	if (a >= b && b >= c)
 	{
 		largest = a;
 	}
-	else if (b > a && b > c)
+	else if (b >= a && a >= c)
 	{
 		largest = b;
 	}
-	else
+	else if (a == c && b > a)
+	{
+		largest = b;
+	}
+	else if (b <= a && a < c)
 	{
 		largest = c;
+	}
+	else if (c <= b && b < a)
+	{
+		largest = a;
+	}
+	else if (c == b && b > a)
+	{
+		largest = c;
+	}
+	else if (c > a && c == b)
+	{
+		largest = c;
+	}
+	else if (a >= c && b > c)
+	{
+		largest = b;
+	}
+	else if (b >= c && a > b)
+	{
+		largest = a;
 	}
 	return (largest);
 }

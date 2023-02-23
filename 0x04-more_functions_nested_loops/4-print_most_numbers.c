a9ca1e9e64869841036ed9e81b789b2814309271
myrabel <memusi2005@gmail.com>
2023-02-23 11:59:26 -0800

    print all except 2 & 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print all except 2 and 4
+ *
+ * Return: Always 0
+ */
+
+void print_most_numbers(void);
+
+/**
+ * print_most_numbers - print all except 2 and 4
+ *
+ * Return: Always 0
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar(i + '0');
+		}
+		_putchar('\n');
+	}
+}

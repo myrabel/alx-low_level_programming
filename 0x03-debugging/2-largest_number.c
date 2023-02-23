ea2696b001f28ccc1881d816c77a268d7d2ee0e6
myrabel <memusi2005@gmail.com>
2023-02-23 08:40:33 -0800

    New push

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,41 +12,41 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a >= b && b >= c)
+	if (a > b && b > c)
 	{
 		largest = a;
 	}
-	else if (b >= a && a >= c)
+	else if (b > a && b > c)
 	{
 		largest = b;
 	}
-	else if (a == c && b > a)
-	{
-		largest = b;
-	}
-	else if (b <= a && a < c)
+	else if (c > a && c > b)
 	{
 		largest = c;
 	}
-	else if (c <= b && b < a)
+	else if (a == b && a > c)
 	{
 		largest = a;
 	}
-	else if (c == b && b > a)
-	{
-		largest = c;
-	}
-	else if (c > a && c == b)
-	{
-		largest = c;
-	}
-	else if (a >= c && b >= c)
+	else if (b == c && b > a)
 	{
 		largest = b;
 	}
-	else if (b >= c && a >= b)
+	else if (a == c && c > b)
+	{
+		largest = c;
+	}
+	else if (a == b && b > c)
+	{
+		largest = b;
+	}
+	else if (b == c && a > c)
 	{
 		largest = a;
+	}
+	else if (a == c && c > b)
+	{
+		largest = c;
 	}
 	return (largest);
 }

b149428da808c1e7d77f4f71c4a2ae7fa8ec5f3c
myrabel <memusi2005@gmail.com>
2023-02-23 13:34:09 -0800

    triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * print_triangle - print triangle
+ * @size: variable
+ * Description: print triangle
+ * Return: Always 0
+ */
+
+void print_triangle(int size);
+
+/**
+ * print_triangle - print triangle
+ * @size: variable
+ * Description: print triangle
+ * Return: Always 0
+ */
+
+void print_triangle(int size)
+{
+	int i, j, hash;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i <= size; i++)
+		{
+			for (j = size - i; j > 0; j--)
+			{
+				_putchar(' ');
+			}
+			for (hash = 0; hash < i; hash++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}

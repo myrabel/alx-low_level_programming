09964ea8b7781a28dfa487de316d77fbbad0b0bc
myrabel <memusi2005@gmail.com>
2023-02-28 02:53:31 -0800

    Length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include "string.h"
+#include <stdio.h>
+/**
+ * _strlen - return length of string
+ * @s: character
+ * Return: Always 0
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(s);
+	return (len);
+}

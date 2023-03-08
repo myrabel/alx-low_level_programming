dfed0971c52556d39e5f93e6ab49171efe17f79a
myrabel <memusi2005@gmail.com>
2023-03-08 10:08:21 -0800

    Print string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - print string followed by new line
+ * @s: variable
+ * Return: Always 0
+ */
+
+void _puts_recursion(char *s)
+{
+	puts(s);
+}

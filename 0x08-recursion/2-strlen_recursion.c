a6c52aacdef60e2a37f015dc3a654d013270f013
myrabel <memusi2005@gmail.com>
2023-03-08 10:23:12 -0800

    Print lenght of string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen_recursion - return length of a string
+ * @s: variable
+ * Return: length of string
+ */
+
+int _strlen_recursion(char *s)
+{
+	int len;
+
+	len = strlen(s);
+	return (len);
+}

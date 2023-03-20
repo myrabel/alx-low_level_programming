7eddaed669953aabafe272b08b0d64bfb487a32f
myrabel <memusi2005@gmail.com>
2023-03-20 04:48:23 -0700

    Create a struct for Poppy the dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure created
+ * @name: dog name
+ * @owner: dog owner
+ * @age: dog age
+ */
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+#endif

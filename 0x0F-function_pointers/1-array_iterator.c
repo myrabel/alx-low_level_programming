69ba0685717d877836d1e16c0900fdf9d66a1dee
myrabel <memusi2005@gmail.com>
2023-03-22 02:49:30 -0700

    Array iterator to each element

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * array-iterator - execute a function on each array element
+ * @array: array
+ * @size: size of array
+ * @action: action
+ * Return: result
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i = 0;
+
+	if (action == NULL || array == NULL)
+		return;
+
+	for (; i < size; i++)
+		action(array[i]);
+}

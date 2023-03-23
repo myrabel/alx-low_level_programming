1159d552501a985f0badfa899d571f6dcc35d6b4
myrabel <memusi2005@gmail.com>
2023-03-23 11:52:05 -0700

    print strings followed by a newline

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -21,7 +21,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	{
 		string = va_arg(ap, char*);
 		if (string == NULL)
-			printf("nil");
+			printf("(nil)");
 		else
 			printf("%s", string);
 		if (separator == NULL)
